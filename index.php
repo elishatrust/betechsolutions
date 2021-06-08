0bc0aafcdffd9b8f79447051a9f018e5872e9092
geeltech <bwilukiroelisha@gmail.com>
2021-06-08 16:52:18 +0300

    Final commit

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>BeTech Solutions| Software Development Company</title>
+    <meta name="description" content="BeTech Solutions Brings You Software Products And Services For Building The Future" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!--bootstrap file-->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!--jquery file-->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<!--fontawaresome file-->
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Finger+Paint&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Allerta+Stencil&display=swap" rel="stylesheet">
+	<!-- css file-->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!---javascript file-->
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<!---NavigationBar-->
+<section id="nav-bar">
+	<nav class="navbar navbar-expand-lg">
+  <a href="index.php" class="brand">
+	  <h1 class="logo-text" style="font-size:33px!important;">
+	  <span style="font-family:'Allerta Stencil',sans-serif; color:white;">Be</span>Tech</h1>
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"><small class="text-white">Menu</small>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+		<li class="nav-item">
+		  <a class="nav-link" href="#index">HOME</a>
+		</li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about">ABOUT</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#services">SERVICES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#mss">MSS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#contact">CONTACT</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</section>
+<!---Get font page-->
+<section class="font-page">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 font-page-text">
+				<span style="color: #00008b;font-family: 'Finger Paint', cursive;">BeTech</span>Solutions<br> Focus On Quality And Cost-Effective Of Modern Solutions.<br>
+				<a href="#contact" class="btn btn-primary contactButton" style="border-radius: 10%;">Get Started <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+			</div>
+			<div class="col-md-6 ">
+				<div class="font-page-iamge">
+					<img src="picture/image/font-page.gif">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--About-->
+<section id="about">
+	<div class="container">
+		<h2>About</h2><br>
+		<div class="row about-box">
+			<div class="col-md-6">
+				<div class="about-content">
+					<b>Overview</b><br>BeTech Solutions is one of Tanzanian IT company focus on 
+					providing better quality and cost-effective solutions of the individual and/or group company<br>
+					<em> Primary goal</em> is to create value for the organization, 
+					which for most companies means increasing profits.<br>
+					<b>Our Mission</b><br>To use our expertise and professional
+					to generate technolocial soluitons and opportunities
+					of individuals business and organizations.<br><br>
+					<strong>Our Philosophy:</strong><em> Building The Future With Tech.</em><br><hr>
+				</div>
+			</div>
+			<div class="col-md-6 skills-bar">
+				<p>Website Design</p>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 96%;">96%</div>
+				</div>
+				<p>Mobile Development</p>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 87%;">87%</div>
+				</div>
+				<p>Adobe Photoshop</p>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 95%;">95%</div>
+				</div>
+				<p>Graphics</p>
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 90%;">90%</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!---Services-->
+<section id="services">
+	<div class="container">
+		<h2>Our Services</h2>
+		<div class="servicesRow">
+			<div class="row">
+				<div class="col-md-4 text-center serviceContents">
+					<div class="image-icon">
+						<img src="picture/image/webdesign.png" class="img-responsive">
+					</div>
+					<h4>Website Design</h4>
+					<p>We are involving in designing a beautiful 
+					Websites that will help your Business and/or your organization.</p>
+				</div>
+				<div class="col-md-4 text-center serviceContents">
+					<div class="image-icon">
+						<img src="picture/image/mobdev.png" class="img-responsive">
+					</div>
+					<h4>Mobile Development</h4>
+					<p>We develop Mobile Applications that operate 
+						as personal digital Assistance or Mobile Phones.</p>
+				</div>
+				<div class="col-md-4 text-center serviceContents">
+					<div class="image-icon">
+						<img src="picture/image/ecommerce.jpeg" class="img-responsive">
+					</div>
+					<h4>Ecommerce</h4>
+					<p>We build your online store using a flexible, modular platform that allows you to expand.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 text-center serviceContents">
+					<div class="image-icon">
+						<img src="picture/image/branding.jpeg" class="img-responsive">
+					</div>
+					<h4>Branding</h4>
+					<p>A solid brand strategy, logo and guidelines help you to get You recognized.</p>
+				</div>
+				<div class="col-md-4 text-center serviceContents">
+					<div class="image-icon">
+						<img src="picture/image/marketing.jpeg" class="img-responsive">
+					</div>
+					<h4>Marketing</h4>
+					<p>We use strategic marketing tactics that have been proven to work.</p>
+				</div>
+				<div class="col-md-4 text-center serviceContents">
+					<div class="image-icon">
+						<img src="picture/image/seo.jpeg" class="img-responsive">
+					</div>
+					<h4>SEO Optimization</h4>
+					<p>Optimizing our web designs to rank on the first page of google is our speciality.</p>
+				</div>
+			</div>		
+		</div>	
+	</div>
+</section>
+<br><br><br>
+<!---OtherServices-->
+<section id="mss">
+	<div class="container">
+		<h2>Other Services</h2>
+	<div class="row otherservices">
+		<div class="col-md-6">
+			<div class="text-box">
+				<p>Also we provide the best and professional <strong>Management and Secretarial Services - <i>MSS</i></strong> includes:</p>
+				<p class="services-list">
+					<li class="ml-5 p-1">Digital Printing</li>
+					<li class="ml-5 p-1">Internet Surfing</li>
+					<li class="ml-5 p-1">Passport Size</li>
+					<li class="ml-5 p-1">Photocopying</li>
+					<li class="ml-5 p-1">Scanning</li>
+					<li class="ml-5 p-1">Lamination</li>
+					<li class="ml-5 p-1">Binding</li>
+					<li class="ml-5 p-1">Graphic Design</li>
+					<li class="ml-5 p-1">Computer Maintainance</li>
+					<li class="ml-5 p-1">Software Solutions</li>
+				</p>
+				
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="img-box">
+				<img src="picture/image/local-computer.png" class="img-responsive">
+			</div>
+		</div>
+	</div>	
+	</div>
+</section>
+<br><br><br>
+<!---Get in Touch-->
+<section id="contact">
+	<div class="container">
+		<h2>Contact</h2>
+		<div class="row">
+			<div class="col-md-6">
+				<div class="spons">
+					<h3 class="spons-head">Sponsors</h3>
+					<p class="spons-text">If you are interested in sponsoring BeTech, 
+					please email us for pricing and terms</p>
+			
+				</div>
+			</div>
+
+			<div class="col-md-6 contact_info">
+				<div class="follow"><b>Address: </b><i class="fa fa-map-marker"></i>Ilemela - Mwanza, Tanzania</div>
+				<div class="follow"><b>Phone: </b><i class="fa fa-phone"></i>+255(0)653 064 129 | +255(0)769 630 580</div>
+				<div class="follow"><b>Email: </b><i class="fa fa-envelope-o"></i> info@betechsolutions.com</div>
+				<div class="follow"><b>Social: </b>
+					<a href="#"><i class="fa fa-whatsapp social_medial"></i></a>
+					<a href="https://web.facebook.com/elisha.bwilukiro.3"><i class="fa fa-facebook social_medial"></i></a>
+					<a href="https://www.instagram.com/elisha.bwilukiro/?hl=en"><i class="fa fa-instagram social_medial"></i></a>
+					<a href="#"><i class="fa fa-twitter social_medial"></i></a>
+					<a href="https://www.linkedin.com/in/elisha-bwilukiro-744087152/"><i class="fa fa-linkedin social_medial"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!---Footer-->
+<section id="footer">
+	<div class="container">
+		<p>&copy <?php echo date("Y");?> BeTech Solutions. All Rights Reserved <br>| Powered by 
+			<a href="index.php" style="text-decoration: none;color: #FF8C00;">BwilukiroGroup Ltd</a></p>
+	</div>
+</section>
+<!-------Javascript link-------->
+<script src="js/smooth-scroll.js"></script>
+<script>
+	var scroll = new SmoothScroll('a[href*="#"]');
+</script>
+</body>
+</html>
